43805c50afd8e5203117566c10c9ec3f8d47b5c5
DarkAkatsuki <thomas0xken@gmail.com>
2024-12-10 09:14:20 -0500

    Update db.ts

--- config/db.ts
+++ config/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://lightningdev722:sjW86eKb4TttfzQr@cluster0.rq0edjh.mongodb.net/storageX"
+      ""
     );
     // await mongoose.connect("mongodb://127.0.0.1:27017/storx-db");
 
